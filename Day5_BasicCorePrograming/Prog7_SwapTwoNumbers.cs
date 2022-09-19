f1e8b8ecdae6a8b1d6ad1942c3f69d00763672f1
Pallavi <109516737+pallavijawanjal25@users.noreply.github.com>
2022-09-19 15:30:28 +0530

    Update Prog7_SwapTwoNumbers.cs

--- Day5_BasicCorePrograming/Prog7_SwapTwoNumbers.cs
+++ Day5_BasicCorePrograming/Prog7_SwapTwoNumbers.cs
@@ -5,33 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Day5_BasicCorePrograming
+namespace ConsoleAppPracticeBasic_Logical
 {
-static class Prog7_SwapTwoNumbers
+    public static class SwapingNumber
     {
-        public static void SwapNumbers()
+        public static void SwapTwoNo()
         {
-  
-            Console.Write("Enter first number:  ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int a;
+            int b;
+           int temp;
 
-            Console.Write("Enter second number:  ");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.WriteLine("\n\n\nNumbers before swapping -   ");
-            Console.WriteLine("First number :   " + a);
-            Console.WriteLine("Second number :   " + b);
-
-           
-            a = a + b;
-            b = a - b;
-            a = a - b;
-            Console.WriteLine("\n\n\nNumbers after swapping -   ");
-            Console.WriteLine("First number :   " + a);
-            Console.WriteLine("Second number :   " + b);
-
-
-
+            Console.WriteLine("enter value of a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter value of b:");
+            b= int.Parse(Console.ReadLine());
+            Console.WriteLine("before swaping Number is a={0},b={1}",a,b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After Swaping Number is a={0},b={1}",a,b);
+            Console.ReadLine();
 
 
         }
